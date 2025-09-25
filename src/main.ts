376c6ce8cade56927e2a123ab028c49e6cde428d
Charles Prince <charlescprince@gmail.com>
2025-09-25 16:21:58 -0500

    Clean up before starting UI
    Added Vuetify
    Cleaned out template pages and assets
    Removed router (probably put it back, eventually)

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,16 @@
-import './assets/main.css'
-
 import { createApp } from 'vue'
+
+// Vuetify
+import { createVuetify } from 'vuetify'
+import * as components from 'vuetify/components'
+import * as directives from 'vuetify/directives'
+import 'vuetify/styles'
+
 import App from './App.vue'
-import router from './router'
 
-const app = createApp(App)
+const vuetify = createVuetify({
+  components,
+  directives,
+})
 
-app.use(router)
-
-app.mount('#app')
+const app = createApp(App).use(vuetify).mount('#app')
