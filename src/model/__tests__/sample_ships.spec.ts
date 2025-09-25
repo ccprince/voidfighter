b43567b422880c3955e1a0de19406e764691c5b9
Charles Prince <charlescprince@gmail.com>
2025-09-24 23:38:09 -0500

    Add the rest of the sample ships

--- src/model/__tests__/sample_ships.spec.ts
+++ src/model/__tests__/sample_ships.spec.ts
@@ -411,6 +411,429 @@ doShipTest(
 )
 
 //
+// Pirates and Scoundrels of the Sagittarian Frontier
+//
+
+const PiratesAndScoundrels: SampleCase[] = [
+  {
+    ship: Snubfighter({
+      name: 'Wardog',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D8,
+      upgrades: ['Shields', 'Targeting Computer'],
+    }),
+    costWithoutPilot: 9,
+    costWithPilot: 12,
+    printable:
+      'Wardog (snubfighter) 9 (12):2:2d6+1:2d6+1:2d8:Shields,Targeting Computer',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Star Eagle',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D8,
+      upgrades: ['Shields', 'Torpedoes'],
+    }),
+    costWithoutPilot: 9,
+    costWithPilot: 12,
+    printable:
+      'Star Eagle (snubfighter) 9 (12):2:2d6+1:2d6:2d8:Shields,Torpedoes',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Novaburn',
+      speed: 3,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D8,
+    }),
+    costWithoutPilot: 9,
+    costWithPilot: 12,
+    printable: 'Novaburn (snubfighter) 9 (12):3:2d6:2d6:2d8:',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Pirate Snubfighter',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D8, arc: WeaponArc.Front }],
+      pilotBase: Rating.D6,
+    }),
+    costWithoutPilot: 9,
+    costWithPilot: 10,
+    printable: 'Pirate Snubfighter (snubfighter) 9 (10):2:2d6:2d8:2d6:',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Blastar Prototype Snubfighter',
+      speed: 2,
+      weaponsBase: [
+        { firepower: Rating.D8, arc: WeaponArc.Front },
+        { firepower: Rating.D6, arc: WeaponArc.Turret },
+      ],
+      pilotBase: Rating.D10,
+      upgrades: ['Death Flower', 'Shields', 'Targeting Computer'],
+    }),
+    costWithoutPilot: 14, // Book says 19; swapped
+    costWithPilot: 19, // Book says 14
+    printable:
+      'Blastar Prototype Snubfighter (snubfighter) 14 (19):2:2d6+1:2d8+1,2d6+1T:2d10:Death Flower,Shields,Targeting Computer',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Rust Bucket',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D6,
+    }),
+    costWithoutPilot: 7, // Book says 8; swapped with other cost
+    costWithPilot: 8, // Book says 7
+    printable: 'Rust Bucket (snubfighter) 7 (8):2:2d6:2d6:2d6:',
+  },
+  {
+    ship: Gunship({
+      name: 'Gunboat',
+      speed: 2,
+      weaponsBase: [
+        { firepower: Rating.D10, arc: WeaponArc.Front },
+        { firepower: Rating.D6, arc: WeaponArc.Turret },
+      ],
+      pilotBase: Rating.D8,
+      upgrades: ['ECM', 'Shields'],
+    }),
+    costWithoutPilot: 17, // Book says 20; swapped
+    costWithPilot: 20, // Book says 17
+    printable: 'Gunboat (gunship) 17 (20):2:2d8+1:2d10,2d6T:2d8:ECM,Shields',
+  },
+  {
+    ship: Gunship({
+      name: 'Light Freighter',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D8, arc: WeaponArc.Turret }],
+      pilotBase: Rating.D8,
+      upgrades: ['Repair', 'Shields'],
+    }),
+    costWithoutPilot: 13,
+    costWithPilot: 16,
+    printable:
+      'Light Freighter (gunship) 13 (16):2:2d8+1:2d8T:2d8:Repair,Shields',
+  },
+  {
+    ship: Gunship({
+      name: 'Bounty Hunter',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D10, arc: WeaponArc.Front }],
+      pilotBase: Rating.D8,
+      upgrades: ['Mining Charges', 'Shields'],
+    }),
+    costWithoutPilot: 15,
+    costWithPilot: 18,
+    printable:
+      'Bounty Hunter (gunship) 15 (18):2:2d8+1:2d10:2d8:Mining Charges,Shields',
+  },
+  {
+    ship: Gunship({
+      name: 'The Star Griffin - Famous Smuggler',
+      speed: 2,
+      weaponsBase: [
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+      ],
+      pilotBase: Rating.D10,
+      upgrades: ['Agile', 'Maneuverable', 'Repair', 'Shields'],
+    }),
+    costWithoutPilot: 19,
+    costWithPilot: 24,
+    printable:
+      'The Star Griffin - Famous Smuggler (gunship) 19 (24):2:2d8+1:2d8T,2d8T:2d10+1:Agile,Maneuverable,Repair,Shields',
+  },
+  {
+    ship: Corvette({
+      name: 'Pirate Sloop',
+      weaponsBase: [
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+      ],
+      pilotBase: Rating.D6,
+      upgrades: ['Carrier', 'Hard Point', 'Shields'],
+    }),
+    costWithoutPilot: 28,
+    costWithPilot: 29,
+    printable:
+      'Pirate Sloop (corvette) 28 (29):1:2d10+1:2d8T,2d8T,2d8T,2d8T:2d6:Carrier,Hard Point,Shields',
+  },
+  {
+    ship: Corvette({
+      name: 'Medium Freighter',
+      pilotBase: Rating.D6,
+      upgrades: ['ECM'],
+    }),
+    costWithoutPilot: 10,
+    costWithPilot: 11,
+    printable: 'Medium Freighter (corvette) 10 (11):1:2d10::2d6:ECM',
+  },
+]
+
+doShipTest(
+  'Pirates and Scoundrels of the Sagittarian Frontier',
+  PiratesAndScoundrels
+)
+
+//
+// Ships of the Children of Ra
+//
+
+const ChildrenOfRa: SampleCase[] = [
+  {
+    ship: Snubfighter({
+      name: 'Copperhead',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D8,
+      upgrades: ['Agile', 'Maneuverable', 'Fast'],
+    }),
+    costWithoutPilot: 10,
+    costWithPilot: 13,
+    printable:
+      'Copperhead (snubfighter) 10 (13):2:2d6:2d6:2d8+1:Agile,Maneuverable,Fast',
+  },
+  {
+    ship: Snubfighter({
+      name: 'King Cobra',
+      speed: 3,
+      weaponsBase: [{ firepower: Rating.D8, arc: WeaponArc.Front }],
+      pilotBase: Rating.D10,
+      upgrades: ['Agile', 'Maneuverable', 'Fast'],
+    }),
+    costWithoutPilot: 14,
+    costWithPilot: 19,
+    printable:
+      'King Cobra (snubfighter) 14 (19):3:2d6:2d8:2d10+1:Agile,Maneuverable,Fast',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Black Mamba',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D10,
+      upgrades: ['Agile', 'Fast', 'Fully Loaded', 'Maneuverable', 'Stealth'],
+    }),
+    costWithoutPilot: 12, // Book says 17; swapped
+    costWithPilot: 17, // Book says 12
+    printable:
+      'Black Mamba (snubfighter) 12 (17):2:2d6:2d6:2d10+1:Agile,Fast,Fully Loaded,Maneuverable,Stealth',
+  },
+  {
+    ship: Gunship({
+      name: 'Python',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D8, arc: WeaponArc.Front }],
+      pilotBase: Rating.D8,
+      upgrades: ['Decoy', 'ECM', 'Torpedoes', 'Transport'],
+    }),
+    costWithoutPilot: 14,
+    costWithPilot: 17,
+    printable:
+      'Python (gunship) 14 (17):2:2d8:2d8:2d8:Decoy,ECM,Torpedoes,Transport',
+  },
+  {
+    ship: Gunship({
+      name: 'Hognose',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D10, arc: WeaponArc.Front }],
+      pilotBase: Rating.D8,
+      upgrades: ['Shields', 'Targeting Computer', 'Torpedoes'],
+    }),
+    costWithoutPilot: 16,
+    costWithPilot: 19,
+    printable:
+      'Hognose (gunship) 16 (19):2:2d8+1:2d10+1:2d8:Shields,Targeting Computer,Torpedoes',
+  },
+  {
+    ship: Corvette({
+      name: 'Converted Civilian Ship',
+      weaponsBase: [
+        { firepower: Rating.D8, arc: WeaponArc.Front },
+        { firepower: Rating.D8, arc: WeaponArc.Rear },
+      ],
+      pilotBase: Rating.D6,
+      upgrades: ['Tailgunner'],
+    }),
+    costWithoutPilot: 18,
+    costWithPilot: 19,
+    printable:
+      'Converted Civilian Ship (corvette) 18 (19):1:2d10:2d8,2d8R:2d6:Tailgunner',
+  },
+]
+
+doShipTest('Ships of the Children of Ra', ChildrenOfRa)
+
+//
+// Ships of the Celestial Chorus
+//
+
+const CelestialChorus: SampleCase[] = [
+  {
+    ship: Snubfighter({
+      name: 'Cherubim',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D6,
+      upgrades: ['Agile', 'Torpedoes'],
+    }),
+    costWithoutPilot: 9,
+    costWithPilot: 10,
+    printable: 'Cherubim (snubfighter) 9 (10):2:2d6:2d6:2d6+1:Agile,Torpedoes',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Angel',
+      speed: 3,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D6,
+      upgrades: ['Agile', 'Maneuverable'],
+    }),
+    costWithoutPilot: 11,
+    costWithPilot: 12,
+    printable: 'Angel (snubfighter) 11 (12):3:2d6:2d6:2d6+1:Agile,Maneuverable',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Nephilim',
+      speed: 2,
+      weaponsBase: [
+        { firepower: Rating.D8, arc: WeaponArc.Front },
+        { firepower: Rating.D6, arc: WeaponArc.Turret },
+      ],
+      pilotBase: Rating.D6,
+      upgrades: ['Shields', 'Targeting Computer', 'Torpedoes'],
+    }),
+    costWithoutPilot: 14,
+    costWithPilot: 15,
+    printable:
+      'Nephilim (snubfighter) 14 (15):2:2d6+1:2d8+1,2d6+1T:2d6:Shields,Targeting Computer,Torpedoes',
+  },
+  //
+  // This ship is illegal, because only Corvettes are allowed Reinforced Hull
+  //
+  // {
+  //   ship: Gunship({
+  //     name: 'Seraphim',
+  //     speed: 2,
+  //     weaponsBase: [{ firepower: Rating.D10, arc: WeaponArc.Front }],
+  //     pilotBase: Rating.D6,
+  //     upgrades: ['Shields', 'Reinforced Hull', 'Torpedoes', 'Transport'],
+  //   }),
+  //   costWithoutPilot: 16,
+  //   costWithPilot: 17,
+  //   printable:
+  //     'Seraphim (gunship) 16 (17):2:2d8+1:2d10:2d6:Shields,Reinforced Hull,Torpedoes,Transport',
+  // },
+  {
+    ship: Corvette({
+      name: 'Leviathan',
+      weaponsBase: [
+        { firepower: Rating.D10, arc: WeaponArc.Front },
+        { firepower: Rating.D10, arc: WeaponArc.Turret },
+        { firepower: Rating.D10, arc: WeaponArc.Rear },
+      ],
+      pilotBase: Rating.D6,
+      upgrades: ['Carrier', 'Shields', 'Tailgunner'],
+    }),
+    costWithoutPilot: 30,
+    costWithPilot: 31, // Book says 29
+    printable:
+      'Leviathan (corvette) 30 (31):1:2d10+1:2d10,2d10T,2d10R:2d6:Carrier,Shields,Tailgunner',
+  },
+]
+
+doShipTest('Ships of the Celestial Chorus', CelestialChorus)
+
+//
+// Ships of the Cnidarians
+//
+
+const Cnidarians: SampleCase[] = [
+  {
+    ship: Snubfighter({
+      name: 'Squid',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D6,
+    }),
+    costWithoutPilot: 7,
+    costWithPilot: 8,
+    printable: 'Squid (snubfighter) 7 (8):2:2d6:2d6:2d6:',
+  },
+  {
+    ship: Snubfighter({
+      name: 'Barracuda',
+      speed: 2,
+      weaponsBase: [{ firepower: Rating.D6, arc: WeaponArc.Front }],
+      pilotBase: Rating.D6,
+      upgrades: ['Agile', 'Shields', 'Targeting Computer'],
+    }),
+    costWithoutPilot: 10,
+    costWithPilot: 11,
+    printable:
+      'Barracuda (snubfighter) 10 (11):2:2d6+1:2d6+1:2d6+1:Agile,Shields,Targeting Computer',
+  },
+  {
+    ship: Gunship({
+      name: 'Devil Ray',
+      speed: 2,
+      weaponsBase: [
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+        { firepower: Rating.D8, arc: WeaponArc.Turret },
+      ],
+      pilotBase: Rating.D8,
+      upgrades: ['Carrier', 'ECM', 'Shields', 'Torpedoes'],
+    }),
+    costWithoutPilot: 19,
+    costWithPilot: 22,
+    printable:
+      'Devil Ray (gunship) 19 (22):2:2d8+1:2d8T,2d8T:2d8:Carrier,ECM,Shields,Torpedoes',
+  },
+  {
+    ship: Corvette({
+      name: 'Architeuthis',
+      weaponsBase: [
+        { firepower: Rating.D10, arc: WeaponArc.EnhancedTurret },
+        { firepower: Rating.D10, arc: WeaponArc.EnhancedTurret },
+      ],
+      pilotBase: Rating.D8,
+      upgrades: [
+        'Fast',
+        'Enhanced Turret',
+        'Enhanced Turret',
+        'Maneuverable',
+        'Reinforced Hull',
+      ],
+    }),
+    costWithoutPilot: 26,
+    costWithPilot: 29,
+    printable:
+      'Architeuthis (corvette) 26 (29):1:2d10:2d10E,2d10E:2d8:Fast,Enhanced Turret,Enhanced Turret,Maneuverable,Reinforced Hull',
+  },
+  {
+    ship: Corvette({
+      name: "Man O'War",
+      weaponsBase: [{ firepower: Rating.D10, arc: WeaponArc.EnhancedTurret }],
+      pilotBase: Rating.D6,
+      upgrades: ['Carrier', 'Fast', 'Enhanced Turret', 'Shields'],
+    }),
+    costWithoutPilot: 19, // Book says 20; swapped
+    costWithPilot: 20, // Book says 19
+    printable:
+      "Man O'War (corvette) 19 (20):1:2d10+1:2d10E:2d6:Carrier,Fast,Enhanced Turret,Shields",
+  },
+]
+
+doShipTest('Ships of the Cnidarians', Cnidarians)
+
+//
 // Implementation
 //
 
